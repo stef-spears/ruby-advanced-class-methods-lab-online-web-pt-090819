740424874e906b9f907c03283955396e27fae436
Stef Spears <spears.sj87@gmail.com>
2019-10-06 03:51:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,8 +37,9 @@ class Song
   end 
 
   def self.find_or_create_by_name(name)
-    if self.all.include?(name)
-      self.find_by_name(name) else
+    if self.find_by_name(name) 
+        self.find_by_name(name)
+      else
      self.create_by_name(name)
     end 
   end 
