cba5129d530aee757bdaec28b88c8d60fb285c69
Stef Spears <spears.sj87@gmail.com>
2019-10-06 03:36:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,11 +12,17 @@ class Song
     self.class.all << self
   end
 
-def self.create 
-  song = self.new 
-  song.save
-  song
-end 
+  def self.create 
+    song = self.new 
+    song.save
+    song
+  end 
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end 
 
  def self.create_by_name(name)
     song = self.new
