a353393b03709d3d021072f9d6ec2b369942f88b
Stef Spears <spears.sj87@gmail.com>
2019-10-06 01:55:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -10,4 +12,24 @@ class Song
     self.class.all << self
   end
 
+def self.create 
+  song = self.new 
+  song.save
+  song
+end 
+
+ def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    song.save
+    song
+  end
+# binding.pry
+
+def self.destroy_all
+  @@all.clear
+end 
+
 end
+
+
